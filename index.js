56e40897a53d872acccc3a5f014d0edaa485181f
najim2004 <najim10112004@gmail.com>
2024-04-26 00:36:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// ----------------import--------------------
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// ----------------import--------------------
+
+// ---------------------middleware--------------------
+app.use(cors());
+app.use(express());
+// ---------------------middleware--------------------
